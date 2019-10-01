ee72909e8819f3527c455df232183e90175016bd
devendra <dev225.reddys@gmail.com>
2019-10-02 03:38:33 +0530

    Changes as Discussed on OCT-1

--- hci_json.py
+++ hci_json.py
@@ -16,65 +16,158 @@ def readJson(fileName):
     getJsonData = json.load(readFile)
     return getJsonData
 
-def getDirs(jsonData):
-    Dirs = []
-    for obj in range(len(jsonData)):
-        Dirs.append(jsonData[obj]['module'][0]['path']['local_path'])
+def svn_update(dire):
+    if path.isdir(dire):
+        print("Found HCI Directory.\n Updating it..")
+        callSVN = sp.getoutput('svn update '+dire)
+        print(callSVN)
+    else:
+        print("It's not a Directory (or) Can't Find an HCI Directory with : ",dire)
+
+def svn_delete(dire):
+    if path.isdir(dire):
+            print("Found HCI Directory.\nRemoving..")
+            shutil.rmtree(dire)
+            print('Directory -> {0} has been removed Succesfully.'.format(dire))
+    else:
+        print("It's not a Directory (or) Can't Find an HCI Directory with : ",dire)
+
+def getDirs(jsonData,keyList):
+    Dirs = {}
+    for keyi in range(len(keyList)):
+        key = keyList[keyi]
+        Dirs[key] = []
+        if len(jsonData['module'][key]['path']) > 1:
+            for dir in range(len(jsonData['module'][key]['path'])):
+                Dirs[key].append(jsonData['module'][key]['path'][dir]['local_path'])
+                # Dirs.append(jsonData['module'][key]['path'][dir]['local_path'])
+        else:
+            Dirs[key].append(jsonData['module'][key]['path'][0]['local_path'])
     return Dirs
-def getDirUrlLbl(jsonData):
-    Dirs = []
-    url = []
-    keyName = []
-    for obj in range(len(jsonData)):
-        Dirs.append(jsonData[obj]['module'][0]['path']['local_path'])
-        url.append(jsonData[obj]['module'][0]['path']['remote_path'])
-        keyName.append(jsonData[obj]['module'][0]['name'])
-    return Dirs,url,keyName
+
+def getAllDat(jsonData,keyList):
+    AllDat = {}
+    for keyi in range(len(keyList)):
+        key = keyList[keyi]
+        if len(AllDat) ==0 or key not in AllDat.keys():
+            AllDat[key] = {}
+            if "local_path" not in AllDat[key].keys() or len(AllDat[key]) <=1:
+                AllDat[key]["local_path"] = []
+            if "remote_path" not in AllDat[key].keys():
+                AllDat[key]["remote_path"] = []
+        if len(jsonData['module'][key]['path']) > 1:
+            for dir in range(len(jsonData['module'][key]['path'])):
+                AllDat[key]["local_path"].append(jsonData['module'][key]['path'][dir]['local_path'])
+                AllDat[key]["remote_path"].append(jsonData['module'][key]['path'][dir]['remote_path'])
+                # Dirs.append(jsonData['module'][key]['path'][dir]['local_path'])
+        else:
+            AllDat[key]["local_path"].append(jsonData['module'][key]['path'][0]['local_path'])
+            AllDat[key]["remote_path"].append(jsonData['module'][key]['path'][0]['remote_path'])
+    return AllDat
+
+def getkeyData(jsonData,PasskeyV):
+    keyDat = {}
+    key = PasskeyV
+    if len(keyDat) ==0 or key not in keyDat.keys():
+        keyDat[key] = {}
+        if "local_path" not in keyDat[key].keys() or len(keyDat[key]) <=1:
+            keyDat[key]["local_path"] = []
+        if "remote_path" not in keyDat[key].keys():
+            keyDat[key]["remote_path"] = []
+    if len(jsonData['module'][key]['path']) > 1:
+        for dir in range(len(jsonData['module'][key]['path'])):
+            keyDat[key]["local_path"].append(jsonData['module'][key]['path'][dir]['local_path'])
+            keyDat[key]["remote_path"].append(jsonData['module'][key]['path'][dir]['remote_path'])
+            # Dirs.append(jsonData['module'][key]['path'][dir]['local_path'])
+    else:
+        keyDat[key]["local_path"].append(jsonData['module'][key]['path'][0]['local_path'])
+        keyDat[key]["remote_path"].append(jsonData['module'][key]['path'][0]['remote_path'])
+    return keyDat
+
+def getDirUrlLbl(jsonData,keyList):
+    Dirs = {}
+    url = {}
+    # keyName = []
+    for keyi in range(len(keyList)):
+        key = keyList[keyi]
+        Dirs.append(jsonData[0]['module'][key]['path'][0]['local_path'])
+        url.append(jsonData[0]['module'][key]['path'][0]['remote_path'])
+        # keyName.append(jsonData[obj]['key'][0]['name'])
+    return Dirs,url
 
 def getURL():
     url = []
     for obj in range(len(jsonData)):
-        url.append(jsonData[obj]['module'][0]['url'])
+        url.append(jsonData[obj]['key'][0]['url'])
     return url
 
-def _RESET_(jsonData):
-    Dirs = getDirs(jsonData)
-    for Dire in range(len(Dirs)):
-        if path.isdir(Dirs[Dire]):
-            print("Found HCI Directory.\nRemoving..")
-            shutil.rmtree(Dirs[Dire])
-            print('Directory -> {0} has been removed Succesfully.'.format(Dirs[Dire]))
+def _RESET_(jsonData,keyList):
+    Dirs = getDirs(jsonData,keyList)
+    for key_value in Dirs.keys():
+        if len(Dirs[key_value]) > 1:
+            for dire in range(len(Dirs[key_value])):
+                svn_delete(Dirs[key_value][dire])
         else:
-            print("It's not a Directory (or) Can't Find an HCI Directory with : ",Dirs[Dire])
+            svn_delete(Dirs[key_value][0])
 
-def _Update_(jsonData):
-    Dirs = getDirs(jsonData)
-    for Dire in range(len(Dirs)):
-        if path.isdir(Dirs[Dire]):
-            print("Found HCI Directory.\n Updating it..")
-            callSVN = sp.getoutput('svn update '+Dirs[Dire])
-            print(callSVN)
+def _Update_(jsonData,keyList):
+    Dirs = getDirs(jsonData,keyList)
+    for key_value in Dirs.keys():
+        if len(Dirs[key_value]) > 1:
+            for dire in range(len(Dirs[key_value])):
+                svn_update(Dirs[key_value][dire])
         else:
-            print("It's not a Directory (or) Can't Find an HCI Directory with : ",Dirs[Dire])
+            svn_update(Dirs[key_value][0])
+
+def svn_co(keyV, local_path, remote_path):
+    if path.isdir(local_path):
+        print("Path {0} -> {1} already exists. Skipping..".format(keyV,local_path))
+    else:
+        print("Checking out :  \t {0} -> {1}: ".format(keyV,local_path))
+        callSVN = sp.getoutput('svn co '+remote_path+' '+local_path)
+        print(callSVN)
+
 def checkAll(jsonData,keyList):
-    Dirs,url,keyName = getDirUrlLbl(jsonData)
-    for dire,eurl,keyv in zip(Dirs,url,keyName):
-        if path.isdir(dire):
-            print("Path {0} -> {1} already exists. Skipping..".format(keyv,dire))
+    getAllDat1 = getAllDat(jsonData,keyList)
+    # print(getAllDat1.keys())
+    # exit()
+    for keyV in getAllDat1.keys():
+        if len(getAllDat1[keyV]["local_path"]) > 1:
+            for pos in range(len(getAllDat1[keyV]["local_path"])):
+                keyData = keyV
+                local_path = getAllDat1[keyV]["local_path"][pos]
+                remote_path = getAllDat1[keyV]["remote_path"][pos]
+                svn_co(keyV,local_path,remote_path)
         else:
-            print("Checking out :  \t {0} -> {1}: ".format(keyv,dire))
-            callSVN = sp.getoutput('svn co '+eurl+' '+dire)
-            print(callSVN)
+            keyData = keyV
+            local_path = getAllDat1[keyV]["local_path"][0]
+            remote_path = getAllDat1[keyV]["remote_path"][0]
+            svn_co(keyV,local_path,remote_path)
+
+    # for dire,eurl,keyv in zip(Dirs,url,keyList):
+    #     if path.isdir(dire):
+    #         print("Path {0} -> {1} already exists. Skipping..".format(keyv,dire))
+    #     else:
+    #         print("Checking out :  \t {0} -> {1}: ".format(keyv,dire))
+    #         callSVN = sp.getoutput('svn co '+eurl+' '+dire)
+    #         print(callSVN)
 def checkKey(jsonData,PasskeyV):
-    Dirs,url,keyName = getDirUrlLbl(jsonData)
-    for dire,eurl,keyv in zip(Dirs,url,keyName):
-        if keyv == PasskeyV:
-            if path.isdir(dire):
-                print("Path {0} -> {1} already exists. Skipping..".format(keyv,dire))
-            else:
-                print("Checking out :  \t {0} -> {1}: ".format(keyv,dire))
-                callSVN = sp.getoutput('svn co '+eurl+' '+dire)
-                print(callSVN)
+    keyData = getkeyData(jsonData,PasskeyV)
+    if len(keyData[PasskeyV]["local_path"]) > 1:
+        for pos in range(len(keyData[PasskeyV]["local_path"])):
+            local_path = keyData[PasskeyV]["local_path"][pos]
+            remote_path = keyData[PasskeyV]["remote_path"][pos]
+            svn_co(PasskeyV,local_path,remote_path)
+    else:
+        local_path = keyData[PasskeyV]["local_path"][0]
+        remote_path = keyData[PasskeyV]["remote_path"][0]
+        svn_co(PasskeyV,local_path,remote_path)
+    # if path.isdir(dire):
+    #     print("Path {0} -> {1} already exists. Skipping..".format(keyv,dire))
+    # else:
+    #     print("Checking out :  \t {0} -> {1}: ".format(keyv,dire))
+    #     callSVN = sp.getoutput('svn co '+eurl+' '+dire)
+    #     print(callSVN)
 
 
 if __name__ == "__main__":
@@ -91,15 +184,17 @@ if __name__ == "__main__":
         parser.print_help
     else:    
         filePath = args.JsonFile
-        jsonDataRaw = readJson(filePath)
-        jsonDataV = jsonDataRaw[0]
-        jsonData = jsonDataRaw[1:] 
-        keyList = [jsonData[i]['module'][0]['name'] for i in range(len(jsonData))]
+        jsonData = readJson(filePath)
+        # keyList = [jsonData[i]['key'][0]['name'] for i in range(len(jsonData))]
+        keyList = list(jsonData['module'].keys())
+        # print(keyList)
+        # print((jsonData['module']['ap3']["path"][1]))
+        # exit()
         # print(args.Command)
         if args.Command in updateCommands:
-            _Update_(jsonData)
+            _Update_(jsonData,keyList)
         elif args.Command in deleteCommands:
-            _RESET_(jsonData)
+            _RESET_(jsonData,keyList)
         elif (args.Command == 'ALL') or (args.Command == 'all'):
             checkAll(jsonData,keyList)
         elif (args.Command in keyList):

36c3bf8ed0b78021a065f66bae0d3e7b0ab3a63f
devendra <dev225.reddys@gmail.com>
2019-11-12 00:29:34 +0530

    Few Commits, But way more to go

--- svn_scm_obj_oct31/app.py
+++ svn_scm_obj_oct31/app.py
@@ -1,9 +1,14 @@
 from modules.scm import import scm
 from modules import rjson
+import pysvn
 
 
-Git = Git()
-SVN = SVN()
-class scm_obj():
-    def __init__(self, scm_type):
-        self.type = scm_type
+# Git = Git()
+# SVN = SVN()
+# class scm_obj():
+#     def __init__(self, scm_type):
+#         self.type = scm_type
+
+client = pysvn.Client()
+client.callback_ssl_server_trust_prompt = ssl_server_trust_prompt
+client.callback_get_login = get_login
--- svn_scm_obj_oct31/modules/scm.py
+++ svn_scm_obj_oct31/modules/scm.py
@@ -1,7 +1,90 @@
 
+# class scm():
+#     def __init__(self, url, path, option):
+#         self.url = url
+#         self.path = path
+#         self.option = option
+#     def Git(self, **kwargs):
+#         print(self.url)
+#         print("called Git")
+#     def svn(self, **kwargs):
+#         print(remote_url)
+#         print(self.url)
+#         print("Called SVN")
+
+# class scm_type():
+#     def __init__(self, scm_type):
+#         self.scm_type = scm_type
+#         actions = {
+#             "checkout":scm("scm_type").checkout("remote_url", "local_url")
+#         }
+
+# # t = "svn"
+# # a = scm("SomeURL", "SomePath", "someOption")
+# # ClBindings = {"svn":a.SVN,"git":a.Git}
+# # ClBindings[t]
+
+# # scm.Git()
+# scm_type(svn)
+
+
+
+# # class scm():
+# #     def __init__(self, scm_type, url, path):
+# #         self.scm_type = scm_type
+# #         self.url = url
+# #         self.path = path
+# #         # self.option = optionx 
+# #         print("init")
+# #         (self.scm_type)(url, path)
+# #     def Git(self):
+# #         print(self.url)
+# #         print("called Git")
+# #     def svn(self):
+# #         print(self.url)
+# #         print("Called SVN")
+
+# # scm("svn", "SomeURL", "SomePath")
+
+
+# class scm():
+#     def __init__(self):
+#         pass
+#     def checkout():
+#         pass
+#     def update():
+#         pass
+
+class git():
+    def __init__(self):
+        self.url = scm.url
+        self.path = scm.path
+        self.option = scm.option
+        print("git",self.url)
+class svn():
+    def __init__(self):
+        self.url = None
+        self.path = None
+        self.option = None
+        print("svn", self.url)
+    def update(self, rem_url, local_path):
+        self.rem_url = rem_url
+        self.local_path = local_path
+        print("test")
+    def checkout(self, rem_url, local_path):
+        self.rem_url = rem_url
+        self.local_path = local_path
+
 class scm():
     def __init__(self, scm_type, url, path, option):
-        def Git():
-            pass
-        def SVN():
-            pass
+        self.url = url
+        self.path = path
+        self.option = option
+        funcs = {
+            "svn":svn,
+            "git":git
+        }
+        scm_obj = (funcs[scm_type])
+        call_scm = scm_obj.(self.option)(self.url,self.path)
+
+scm("svn", "someR", "somep", "update")
